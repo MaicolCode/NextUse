ef3fa179a29b651885b0323c49023b93f733c5ab
MaicolCode <paucarjhon07@outlook.com>
2024-02-18 22:16:19 -0500

    Se realizan los nuevo cambios dentro de los directorios y ficheros

---
+++ components/Home/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Hero() {
+    return (
+        <section>
+            <div className='row'>
+                <section className='col-lg-6'>
+                    <h1 className='hero__title'>Hey There, I am Maicol</h1>
+                </section>
+            </div>
+        </section>
+    )
+}
--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,1 +1,14 @@
+<<<<<<< HEAD
+import Layout from '@/components/Layout'
+import React from 'react'
+
+export default function Nosotros() {
+    return (
+        <Layout title='nosotros' description={'Descripción nosotros'}>
+            <h1>Nosotros</h1>
+        </Layout>
+    )
+}
+=======
 console.log("Se inserta un nuevo cambio ")
+>>>>>>> 9196b7f6ab1a3cf47534eb1b6070ae2ee2008d99
