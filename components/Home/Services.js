d7e7d3f94c45dc1e6cdb38aa227bd754fe0fc169
MaicolCode <paucarjhon07@outlook.com>
2024-02-19 22:24:40 -0500

    Se aplican los cambios dentro de los estilos con los breakpoints y el nuevo directorio Services

---
+++ components/Home/Services.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Services() {
+    return (
+        <div className='contanier'>
+            <p>Aquí va el contenido de los servicios</p>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 import AboutMe from '@/components/Home/AboutMe';
 import Layout from '@/components/Layout';
 import Hero from '@/components/Home/Hero';
+import Services from '@/components/Home/Services';
 import Head from "next/head";
 
 export default function Home() {
   return (
     <Layout title='Home' description={'Descripción home'}>
       <main>
-        <Hero />
+        <Hero />        
         <AboutMe />
+        <Services />
       </main>
     </Layout>
   );
